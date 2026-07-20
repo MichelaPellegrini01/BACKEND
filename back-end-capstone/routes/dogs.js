1f4e4d3a4b8cf7808e9ea1949b6caaa9f0257867
Il Tuo Nome <latuaemail@example.com>
2026-07-20 18:14:30 +0200

    commenti backend

--- back-end-capstone/routes/dogs.js
+++ back-end-capstone/routes/dogs.js
@@ -1,10 +1,11 @@
 import express from "express";
+import upload from "../config/multer.js";
 import { getDogs, createDog, getDogById, updateDog, deleteDog } from "../controllers/dogController.js";
 
 const router = express.Router();
 router.get("/", getDogs);
 router.get("/:id", getDogById);
-router.post("/", createDog);
-router.put("/:id", updateDog);
+router.post("/", upload.single("image"), createDog);
+router.put("/:id", upload.single("image"), updateDog);
 router.delete("/:id", deleteDog);
 export default router;
